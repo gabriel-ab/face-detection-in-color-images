97d7b5e0fc3a3c35d13c06cce2bf3d7effae3fd2
Gabriel Barbosa <gabriel.a07b@gmail.com>
2023-11-14 19:22:02 -0300

    feat: skin color attempt

--- face_detection/skin_color_detection.py
+++ face_detection/skin_color_detection.py
@@ -1,9 +1,64 @@
 import numpy as np
+import cv2 as cv
 
 
 a = np.sin(2.53)
 b = np.cos(2.53)
 skin_model = np.array([[ b, a],[-a, b]])
+cx = 109.38
+cy = 152.02
+
 
 def skin_color_detection(ycc: np.ndarray):
-    xy = skin_model
+    cb = ycc[..., 2] - cx
+    cr = ycc[..., 1] - cy
+    xy = skin_model @ [cb,cr]
+    return xy
+
+
+
+def my_skin_color_detection(image: np.ndarray, ycc: np.ndarray = None, inplace: bool = False):
+    ymin = 16.0
+    ymax = 235.0
+    crmin = 142.0
+    crmax = 162.0
+    cbmin = 100.0
+    cbmax = 119.0
+
+    if image.dtype == np.float32:
+        image = np.uint8(image * 255)
+
+    if ycc is None:
+        ycc = cv.cvtColor(image, cv.COLOR_RGB2YCR_CB)
+
+    y, cr, cb = np.moveaxis(ycc, 2, 0)
+
+    valid_pixels = (
+        ((ymin < y) & (y < ymax))
+        & ((crmin < cr) & (cr < crmax))
+        & ((cbmin < cb) & (cb < cbmax))
+    )
+    result = image if inplace else image.copy()
+    result[~valid_pixels] = 0
+    return result
+
+
+def aproximation_skin_color(image: np.ndarray, ycc: np.ndarray = None, inplace: bool = False):
+    ymin = 16.0
+    ymax = 235.0
+    crmin = 142.0
+    crmax = 162.0
+    cbmin = 100.0
+    cbmax = 119.0
+
+    if image.dtype == np.float32:
+        image = np.uint8(image * 255)
+
+    if ycc is None:
+        ycc = cv.cvtColor(image, cv.COLOR_RGB2YCR_CB)
+
+    y, cr, cb = np.moveaxis(ycc, 2, 0)
+
+    ylim = ((ymin < y) & (y < ymax))
+    crlim = ((crmin < cr) & (cr < crmax))
+    cblim = ((ymin < y) & (y < ymax))
--- visualization/skin_color_detection.py
+++ visualization/skin_color_detection.py
@@ -1,18 +1,35 @@
 """Teste visual da detecção de Pele"""
 
 from matplotlib import pyplot as plt
+from matplotlib import widgets as wid
 from pathlib import Path
 import cv2 as cv
-
-from face_detection.skin_color_detection import my_skin_color_detection
+import numpy as np
+from face_detection.light_compensation import light_compensation
+from face_detection.color_space_transformation import color_space_transformation
 
 image_path = Path(__file__).parent / 'reference.png'
 print(image_path)
 
+def skin_color_detection(ycc):
+    cr = ycc[..., 1]
+    cb = ycc[..., 2]
+    cr_selection = (130 < cr) & (cr < 155)
+    cb_selection = (80 < cb) & (cb < 140)
+    return cr_selection | cb_selection
+
+
 image = plt.imread(image_path)
-result = my_skin_color_detection(image)
+image2 = light_compensation(image)
+image3 = cv.cvtColor(image2, cv.COLOR_RGB2YCR_CB)
+image4 = color_space_transformation(image3)
+image5 = image2.copy()
+image5[~skin_color_detection(image4)] = 0
 
 plt.figure(figsize=(14,10))
-plt.subplot(121, title='Original').imshow(image).axes.axis(False)
-plt.subplot(122, title='After').imshow(result).axes.axis(False)
+plt.subplot(151, title='1').imshow(image).axes.axis(False)
+plt.subplot(152, title='2').imshow(image2).axes.axis(False)
+plt.subplot(153, title='3').imshow(image3).axes.axis(False)
+plt.subplot(154, title='4').imshow(image4).axes.axis(False)
+plt.subplot(155, title='5').imshow(image5).axes.axis(False)
 plt.show()
