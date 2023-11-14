07ec5923287f7fe0d8d4e90d9e7b9c2f9c1083f6
Gabriel Barbosa <gabriel.a07b@gmail.com>
2023-11-14 12:05:28 -0300

    refactor: change var name

--- visualization/light_compensation.py
+++ visualization/light_compensation.py
@@ -7,9 +7,9 @@ from face_detection.light_compensation import light_compensation
 image_path = Path(__file__).parent / 'reference.png'
 
 image = plt.imread(image_path)
-lux_corrected_image = light_compensation(image)
+result = light_compensation(image)
 
 plt.figure(figsize=(14,10))
 plt.subplot(121, title='Original').imshow(image).axes.axis(False)
-plt.subplot(122, title='Corrected').imshow(lux_corrected_image).axes.axis(False)
+plt.subplot(122, title='Corrected').imshow(result).axes.axis(False)
 plt.show()
