b20a90c00d2982c54f3c63eaf043517227ecb0ce
Gabriel Barbosa <gabriel.a07b@gmail.com>
2023-12-11 14:17:00 -0300

    update skin color detection

--- face_detection/skin_color_detection.py
+++ face_detection/skin_color_detection.py
@@ -9,10 +9,10 @@ def skin_color_detection(image):
     ycrcb = cv.cvtColor(image, cv.COLOR_RGB2YCR_CB)
 
     # (height, width, 2) -> (2, height, width) -> (2, height * width) = ([cr,cb], num_pixels)
-    crcb_points = np.swapaxes(ycrcb[..., 1:3], 0, -1).reshape(2, -1)
+    crcb_points = np.swapaxes(ycrcb, 0, -1).reshape(3, -1)
 
     # Calculating if is inside cluster of skin tones
-    indices = np.linalg.norm(point - crcb_points, axis=0) < 40
+    indices = np.linalg.norm(point - crcb_points[1:3], axis=0) < 40
     indices = indices.reshape(*ycrcb.shape[:2])
 
     result = image.copy()
