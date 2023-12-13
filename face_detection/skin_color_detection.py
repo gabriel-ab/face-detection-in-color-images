7a57574f13b0578bc503eb872c8e1ca29d308287
Gabriel Barbosa <gabriel.a07b@gmail.com>
2023-12-13 11:23:40 -0300

    update skin color

--- face_detection/skin_color_detection.py
+++ face_detection/skin_color_detection.py
@@ -2,19 +2,28 @@ import numpy as np
 import cv2 as cv
 
 
+ycbcr_points = np.array(([150], [110]))
 
-point = np.array(([150], [110]))
-
-def skin_color_detection(image):
+def ycbcr_skin_detection(image):
     ycrcb = cv.cvtColor(image, cv.COLOR_RGB2YCR_CB)
 
     # (height, width, 2) -> (2, height, width) -> (2, height * width) = ([cr,cb], num_pixels)
     crcb_points = np.swapaxes(ycrcb, 0, -1).reshape(3, -1)
 
     # Calculating if is inside cluster of skin tones
-    indices = np.linalg.norm(point - crcb_points[1:3], axis=0) < 40
+    indices = np.linalg.norm(ycbcr_points - crcb_points[1:3], axis=0) < 40
     indices = indices.reshape(*ycrcb.shape[:2])
+    return indices
 
-    result = image.copy()
-    result[~indices] = 0
-    return result
+
+hsv_lower = np.array([0, 48, 80], dtype=np.uint8)
+hsv_upper = np.array([20, 255, 255], dtype=np.uint8)
+hsv_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+
+def hsv_skin_detection(image: np.ndarray):
+    converted = cv.cvtColor(image, cv.COLOR_RGB2HSV)
+    skin_mask = cv.inRange(converted, hsv_lower, hsv_upper)
+    skin_mask = cv.morphologyEx(skin_mask, cv.MORPH_OPEN, hsv_kernel, iterations=2)
+    # skin_mask = cv.erode(skin_mask, kernel, iterations = 2)
+    # skin_mask = cv.dilate(skin_mask, kernel, iterations = 2)
+    skin_mask = cv.GaussianBlur(skin_mask, (3, 3), 0)
